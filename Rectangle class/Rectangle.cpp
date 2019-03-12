2c6ea51685af8c730a33d8c8eded4866d7e094a6
jmm170130 <47878996+jmm170130@users.noreply.github.com>
2019-03-11 19:41:47 -0500

    Create Rectangle.cpp

---
+++ Rectangle class/Rectangle.cpp
@@ -0,0 +1,21 @@
+//implementation file for the Rectangle class. Contains member function definitions
+#include "Rectangle.h"
+#include <iostream>
+
+using namespace std;
+
+
+//setHeight sets the value of the member variable height
+void Rectangle::setHeight(double h)
+{
+
+	height = h;
+}
+
+
+//setLength sets the value of the member variable length
+void Rectangle::setLength(double len)
+{
+
+	length = len;
+}
